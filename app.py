d5d88959dd53b15ca688b7da9a29941bf0a0f592
Belén Nazareth Durán <belennazareth29@gmail.com>
2022-11-30 04:35:45 +0100

    otra modificacion...

--- app.py
+++ app.py
@@ -5,34 +5,42 @@ from flask import Flask, render_template, request,abort
 
 app = Flask(__name__)
 
+##########################################################################
+
 @app.route('/')
-def inicio():
+def init():
     return render_template("inicio.html")
 
 @app.route('/consulta',methods=["POST"])
-def iniciosesion():
-    usuario=request.form['usuario']
-    contra=request.form['contra']
-    #while True:
-        #try:
-    connection = psycopg2.connect(
-        host="192.168.122.9",
-        database="inter2",
-        user=usuario,
-        password=contra)
-    cursor = connection.cursor()
-    cursor.execute("select deptno from emp")
-    res = cursor.fetchall()        
-    return (str(res))
-        #except psycopg2.DatabaseError:
-        #    return ("Error en el usuario :(")
+def sesion():
     
+    user = request.form['usuario']
+    passwd = request.form['contra']
+   
+    while True:
+        
+        try:
 
-    #return(res)
+            connection = psycopg2.connect(
+                host = "192.168.122.9",
+                database = "inter2",
+                user = user,
+                password = passwd)
 
+            cursor = connection.cursor()
+            cursor.execute("select deptno from emp")
+            row = cursor.fetchall()        
+            return (str(row))
+
+        except psycopg2.DatabaseError:
+            return ("(ノ°益°)ノ EL USUARIO TA HORRIBLE BOLUDO ٩(╬ʘ益ʘ╬)۶ NO EXISTE WACHO (°ㅂ°╬)")
+    
+    return(row)
+
+##########################################################################
 
 #webserver
 
 if __name__ == '__main__':
 
-    app.run(port=int(os.environ.get('PORT', '8080'))) 
+    app.run (port = int(os.environ.get('PORT', '8080'))) 
